580f675192304e8779061a410cc3af0ea3e58a58
nishkalprakash <nishkalprakash@gmail.com>
2022-05-21 18:26:38 +0530

    changed lt1_q1 to match c ode

--- Assignments/LT1/Q1/LT1_Q1.py
+++ Assignments/LT1/Q1/LT1_Q1.py
@@ -1,29 +1,29 @@
-exp=10**5
-ms,hl,li,mi=map(lambda x: float(x)/exp,input("Enter Monthly income, House loan, Life Inurance, Medical Insurance: ").strip().split())
+exp = 10 ** 5
+ms, hl, li, mi = map(
+    lambda x: float(x) / exp,
+    input("Enter Monthly Salary, House loan, Life Inurance, Medical Insurance: ")
+    .strip()
+    .split(),
+)
 
-yi=ms*12 
-tax=0
-
-hl_L=2.5
-li_L=1.5
-mi_L=.5
+yi = ms * 12
 
 if yi >= 10:
-    yi -=  hl_L if hl > hl_L else hl
-    yi -=  li_L if li > li_L else li
-    yi -=  mi_L if mi > mi_L else mi
+    yi -= 2.5 if hl > 2.5 else hl
+    yi -= 1.5 if li > 1.5 else li
+    yi -= 0.5 if mi > 0.5 else mi
 
 if yi > 15:
-    tax+=1.875+.3*(yi-15)
+    tax = 1.875 + 0.3 * (yi - 15)
 elif yi > 12.5:
-    tax+=1.25+.25*(yi-12.5)
+    tax = 1.25 + 0.25 * (yi - 12.5)
 elif yi > 10:
-    tax+=.75+.2*(yi-10)
+    tax = 0.75 + 0.2 * (yi - 10)
 elif yi > 7.5:
-    tax+=.375+.15*(yi-7.5)
+    tax = 0.375 + 0.15 * (yi - 7.5)
 elif yi > 5:
-    tax+=.125+.1*(yi-5)
+    tax = 0.125 + 0.1 * (yi - 5)
 elif yi > 2.5:
-    tax+=.05*yi
-    
+    tax = 0.05 * yi
+
 print(f"Tax = {(tax*exp):.2f}")
