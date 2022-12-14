36dda8f1f4e6d46a119b64f6ff24c26d017987fd
nishkalprakash <nishkalprakash@gmail.com>
2022-12-14 20:44:47 +0530

    Done checking for Q3

--- Assignments/LT1B/Q3/LT1B_Q3_solution.c
+++ Assignments/LT1B/Q3/LT1B_Q3_solution.c
@@ -1,32 +1,26 @@
 // Code creator: Nishkal Prakash (nishkal@iitkgp.ac.in)
 // Program to print largest and 2nd largest at all times.
+// Code update - removed recursion
 
-#include<stdio.h>
-
-int prog(int l, int l2){
-    int N;
-    printf("Enter Number: ");
-    scanf("%d",&N);
-    if(N<0)
-        return 0;
-    if(N>l){
-        if(l!=-1 && l2<l)
-            l2=l;
-        l=N;
-    }
-    else if(N>l2 && N!=l){
-        l2=N;
-    }
-    
-    printf("Largest: %d\n",l);
-    printf("Second largest: ");
-    if(l2==-1)
-        printf("Value Not yet entered\n");
-    else 
-        printf("%d\n",l2);
-    return prog(l,l2);
-}
+#include <stdio.h>
 int main()
 {
-    return prog(-1,-1);
+    int N, l = l2 = -1; // N = temp storage for the number, l=largest, l2=2nd largest
+    printf("Enter Number: ");
+    while (1) // infinite Loop
+    {
+        scanf("%d", &N);
+        if (N < 0)
+            break; // if entered number is negative
+        if (N > l)
+        { // if entered number is largest
+            l2 = l;
+            l = N;
+        }
+        else if (N > l2 && N < l) // if entered number is 2nd largest
+            l2 = N;
+        printf("Largest: %d\nSecond largest: ", l);
+        (l2 == -1) ? printf("Value Not yet entered\n") : printf("%d\n", l2);
+    }
+    return 0;
 }
