93ed90d928e6217f0d4e5777e7ca1cb9d3d621ef
nishkalprakash <nishkalprakash@gmail.com>
2023-01-13 14:39:52 +0530

    done for A8_Q5

--- Assignments/A8/Q5/A8_Q5_solution.c
+++ Assignments/A8/Q5/A8_Q5_solution.c
@@ -1,8 +1,42 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;~~INPUT:~~123~~~~Expected OUTPUT:~~321~~~~;123
-10;~~INPUT:~~101~~~~Expected OUTPUT:~~101~~~~;101
-10;~~INPUT:~~993~~~~Expected OUTPUT:~~399~~~~;993
-10;~~INPUT:~~100~~~~Expected OUTPUT:~~1~~~~;100
-# ```
+// Code creator: Nishkal Prakash (nishkal@iitkgp.ac.in)
+// Program to find Palindromic Substrings
+
+#include <stdio.h>
+int check_palindrome(char *A, int b, int e)
+{ // Checking Palindrome subsring
+    for (int i = 0; i < (e - b + 1) / 2; i++)
+        if (A[b + i] != A[e - i])
+            return 0;
+    return 1;
+}
+
+int print_substring(char *A, int b, int e)
+{ // Printing Palindrome subsring
+    for (int i = b; i <= e; i++)
+        printf("%c", A[i]);
+    printf(", ");
+    return 1;
+}
+int main()
+{
+    char A[100];  // A = Char array
+    int i, j;     // i,j = Loop Vars
+    int l;        // l = Lenth of String in A
+    int flag = 0; // flag = flag to check for at least one palindrome
+
+    printf("Enter a string: ");
+    scanf("%s", A);
+
+    for (; A[++l] != '\0';)
+        ; // Counting String Len of A
+
+    for (i = 0; i < l; i++)
+        for (j = i + 1; j < l; j++)
+            if (check_palindrome(A, i, j))
+                flag = print_substring(A, i, j); // Printing Palindrome subsring
+
+    if (!flag)
+        printf("No palindrome substring present");
+
+    return 0;
+}
