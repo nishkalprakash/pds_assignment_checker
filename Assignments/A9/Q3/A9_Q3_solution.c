47684a94a9a40a8a8fd613317232f17b53706ec3
nishkalprakash <nishkalprakash@gmail.com>
2023-01-19 18:31:30 +0530

    done for q3,q4

--- Assignments/A9/Q3/A9_Q3_solution.c
+++ Assignments/A9/Q3/A9_Q3_solution.c
@@ -1,8 +1,25 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;~~INPUT:~~123~~~~Expected OUTPUT:~~321~~~~;123
-10;~~INPUT:~~101~~~~Expected OUTPUT:~~101~~~~;101
-10;~~INPUT:~~993~~~~Expected OUTPUT:~~399~~~~;993
-10;~~INPUT:~~100~~~~Expected OUTPUT:~~1~~~~;100
-# ```
+// Code creator: Nishkal Prakash (nishkal@iitkgp.ac.in)
+// Program to store a rectange in a structure and calc its area
+
+#include <stdio.h>
+#include <math.h>
+
+typedef struct {
+	int x1,y1,x2,y2;        // (x1,y1) Bottom Left , (x2,y2) Top Right 
+} RECT;
+
+int rect_area(RECT r)   // A Separate func as required by the question
+{
+	int ar=abs((r.x1-r.x2)*(r.y1-r.y2));
+	return ar;
+}
+int main ()
+{
+	RECT R; // R = Rect Structure
+	printf ("Enter the coordinates for Bottom Left: ");
+	scanf("%d%d",&R.x1,&R.y1);
+	printf("Enter the coordinates for Top Right: ");
+	scanf("%d%d",&R.x2,&R.y2);
+	printf("Area:%d",rect_area(R));
+	return 0;
+}
