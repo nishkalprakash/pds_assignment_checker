f1f09977a019b56b97285eb77a1adcdb9a7d6af6
nishkalprakash <nishkalprakash@gmail.com>
2023-01-20 17:14:46 +0530

    uploading final solutions

--- Assignments/A9/Q1/A9_Q1_solution.c
+++ Assignments/A9/Q1/A9_Q1_solution.c
@@ -1,8 +1,43 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;~~INPUT:~~123~~~~Expected OUTPUT:~~321~~~~;123
-10;~~INPUT:~~101~~~~Expected OUTPUT:~~101~~~~;101
-10;~~INPUT:~~993~~~~Expected OUTPUT:~~399~~~~;993
-10;~~INPUT:~~100~~~~Expected OUTPUT:~~1~~~~;100
-# ```
+// Code creator: Abhishek Topwal
+
+#include <stdio.h>
+
+typedef struct complex
+{
+	float real; // real part of the complex number
+	float imag; // imaginary part of the complex number
+}complex;
+
+
+/*****************************************************************************
+ * Add 2 complex numbers
+ * 
+ * @param c1 first complex number
+ * @param c2 second complex number
+ * 
+ * @return a complex number storig the sum of complex numbers c1 and c2
+******************************************************************************/
+complex add(complex c1, complex c2){
+	complex Z;
+	Z.real=c1.real+c2.real;
+	Z.imag=c1.imag+c2.imag;
+	return(Z);
+}
+
+
+int main(){
+	complex Z1, Z2, Z;
+	
+	printf("Enter the real and imaginary part of Z1: ");
+	scanf("%f %f", &Z1.real, &Z1.imag);
+	printf("Enter the real and imaginary part of Z2: ");
+	scanf("%f %f", &Z2.real, &Z2.imag);
+
+	Z=add(Z1, Z2);
+	if(Z.imag>=0)
+		printf("The sum is Z = %.2f + %.2fi", Z.real, Z.imag);
+	else
+		printf("The sum is Z = %.2f %.2fi", Z.real, Z.imag);
+
+	return 0;
+}
