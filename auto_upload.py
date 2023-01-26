48e50922f8965e0fd2823f4e0ae778d7f1817fcd
nishkalprakash <nishkalprakash@gmail.com>
2023-01-26 12:13:33 +0530

    fixed bug of auto_upload

--- auto_upload.py
+++ auto_upload.py
@@ -19,35 +19,38 @@ from lib.pds_selenium import (
     extract_name_roll_tuple,
     init_selenium,
     insert,
+    driver_get_heading_element_list
 )
-from lib.pds_globals import A_Q_, A_Q_PDS_QUIZ_
+from lib.pds_globals import A_Q_
 
 if __name__ == "__main__":
 
-    def upload_to_moodle(a, q):
+    def upload_to_moodle(a, q_topic):
         ## Initialize selenium
 
         ## Get the link to the assignemt_question
 
-        qq = driver_get_topics_from_a(driver,a,q)
-        driver.get(A_Q_PDS_QUIZ_.format(q=qq['q'],qid=qq['qid'],quiz_id=qq['quiz_id']))
+        # driver.get(A_Q_PDS_QUIZ_.format(q=qq['q'],qid=qq['qid'],quiz_id=qq['quiz_id']))
         # driver_get_from_topic(driver, topic_id)
         ## Get each student marks and comments
-        
-        nrt=driver.find_elements(by=By.XPATH,value=h4)
-        cm=driver.find_elements(by=By.XPATH,value=xp%('div','comment_ideditable'))
-        mks=driver.find_elements(by=By.XPATH,value=xp%('input','mark'))
-        max_m=float(driver.find_element(by=By.XPATH,value=xp.replace('@id','@name')%('input','maxmark')).get_attribute('value'))
+        q=q_topic['q']
+        nr_nxt=driver_get_heading_element_list(driver,q_topic)
+        m_elem=nr_nxt[0]['next_div'].find_element(by=By.XPATH,value=xp.replace('@id','@name')%('input','maxmark'))
+        max_m=float(m_elem.get_attribute('value'))
         s_m_c = get_std_roll_to_m_c_dict(a, q,scale=max_m)
+        for i,nrd in enumerate(nr_nxt):
+        # try:
+            next_div=nrd.pop('next_div')
+            r=nrd['r']
+            cm=next_div.find_element(by=By.XPATH,value=xp%('div','comment_ideditable'))
+            mks=next_div.find_element(by=By.XPATH,value=xp%('input','mark'))
         
-        for i,elem in enumerate(nrt):
-            n,r=extract_name_roll_tuple(elem)
-            if not r:
-                r=map_n_r[n]
+        # for i,elem in enumerate(nrt):
+
             ## Upload data to moodle
-            insert(driver,mks[i], f"{s_m_c[r]['m']}")
-            insert(driver,cm[i], f"{s_m_c[r]['c']}")
-            print(f"Done for {r}")
+            insert(driver,mks, f"{s_m_c[r]['m']}")
+            insert(driver,cm, f"{s_m_c[r]['c']}")
+            print(f"{i} - Done for {r}")
         ## Save changes
         driver.find_element(By.XPATH, value="//input[@type='submit' and @value='Save and go to next page']").click()
         sleep(2)
@@ -59,9 +62,10 @@ if __name__ == "__main__":
     ## Go to course page
     driver_get_course(driver)
     map_n_r=get_map_roll_to_name(rev=True)
-    h4="//h4[contains(text(),'Attempt number 1 for ')]"
-    xp=h4+"/following-sibling::div//div[@class='comment']//%s[contains(concat(' ',normalize-space(@id),' '),'-%s')]"
-    for q in ql:
-        upload_to_moodle(a, q)
+    # h4="//h4[contains(text(),'Attempt number 1 for ')]"
+    xp="descendant::div[@class='comment']//%s[contains(concat(' ',normalize-space(@id),' '),'-%s')]"
+    q_topics = [i for i in driver_get_topics_from_a(driver,a) if i['q'] in ql]
+    for q_topic in q_topics:
+        upload_to_moodle(a, q_topic)
     driver.close()
     
