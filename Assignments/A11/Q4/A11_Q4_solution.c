b58da77bb4e8de433bebd5601bf6d06abad66bba
nishkalprakash <nishkalprakash@gmail.com>
2023-02-07 15:43:55 +0530

    formateted document

--- Assignments/A11/Q4/A11_Q4_solution.c
+++ Assignments/A11/Q4/A11_Q4_solution.c
@@ -1,68 +1,81 @@
-#include<stdio.h>
-#include<stdlib.h>
+// Code creator: Abhishek Topwal
+// Program to delete an element from a linked list
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node{
-	int data;
-	struct node* next;
-}node;
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
 
-void printList(node* head){
-    node* temp = head;
-    while(temp!=NULL){
-        printf("%d ",temp->data);
+void printList(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
         temp = temp->next;
     }
     printf("\n");
 }
 
-
 /**
  * @brief Create a linked list
- * 
+ *
  * @param n size of the linked list
  * @param arr includes the element of the linked list
  * @return pointer to the head of the linked list
  */
-node* createList(int n, int *arr){
-    node* head, *temp;
-    for(int i=0; i<n;i++){
-        if(i==0){
-            head = (node*)malloc(sizeof(node));
+node *createList(int n, int *arr)
+{
+    node *head, *temp;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            head = (node *)malloc(sizeof(node));
             head->data = arr[i];
             temp = head;
         }
-        else{
-            temp->next = (node*)malloc(sizeof(node));
+        else
+        {
+            temp->next = (node *)malloc(sizeof(node));
             temp->next->data = arr[i];
             temp = temp->next;
         }
-        if(i==n-1){
+        if (i == n - 1)
+        {
             temp->next = NULL;
         }
     }
     return head;
 }
 
-
 /**
  * @brief delete an element from a linked list and print the remaining list
- * 
+ *
  * @param m element to be deleted
  * @param head head of the linked list
  */
-void deleteElement(int m, node* head){
-    node* temp = head, *prev = head;
-    while(temp!=NULL){
-        if(temp->data==m){
-            //when temp is the only node
-            if(temp==head && temp->next==NULL){
+void deleteElement(int m, node *head)
+{
+    node *temp = head, *prev = head;
+    while (temp != NULL)
+    {
+        if (temp->data == m)
+        {
+            // when temp is the only node
+            if (temp == head && temp->next == NULL)
+            {
                 free(temp);
                 printf("\nElement deleted. The list is now empty.");
                 return;
             }
 
             // when temp is the first node
-            if(temp==head){
+            if (temp == head)
+            {
                 head = temp->next;
                 free(temp);
                 printf("\nElement deleted. The new list is: ");
@@ -79,26 +92,27 @@ void deleteElement(int m, node* head){
         prev = temp;
         temp = temp->next;
     }
-    printf("\n%d does not exist in the list.",m);
+    printf("\n%d does not exist in the list.", m);
 }
 
-
-int main(){
+int main()
+{
     printf("Enter n: ");
-	int n;
-    scanf("%d",&n);
+    int n;
+    scanf("%d", &n);
 
     printf("\nEnter n elements: ");
-    int *arr = malloc(sizeof(int)*n);
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
+    int *arr = malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
     }
 
     printf("\nEnter the element to be deleted: ");
     int m;
-    scanf("%d",&m);
+    scanf("%d", &m);
 
-    node *head = createList(n,arr);
-    deleteElement(m,head);
-	return 0;
+    node *head = createList(n, arr);
+    deleteElement(m, head);
+    return 0;
 }
