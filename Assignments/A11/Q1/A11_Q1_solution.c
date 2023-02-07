4582f424eded91443b84b4156cf6304bf6727722
nishkalprakash <nishkalprakash@gmail.com>
2023-02-07 13:24:41 +0530

    setup for A11_Q1 done

--- Assignments/A11/Q1/A11_Q1_solution.c
+++ Assignments/A11/Q1/A11_Q1_solution.c
@@ -1,8 +1,19 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;~~INPUT:~~123~~~~Expected OUTPUT:~~321~~~~;123
-10;~~INPUT:~~101~~~~Expected OUTPUT:~~101~~~~;101
-10;~~INPUT:~~993~~~~Expected OUTPUT:~~399~~~~;993
-10;~~INPUT:~~100~~~~Expected OUTPUT:~~1~~~~;100
-# ```
+// Code creator: Nishkal Prakash (nishkal@iitkgp.ac.in)
+// Program to Recursively find
+
+#include <stdio.h>
+
+float harmonic(int n)       // Recursive function to calculate the sum of series
+{
+    if (n == 1) return (1);
+    return (1.0 / n + harmonic(n - 1));
+}
+
+int main()
+{
+    int N;  // N = The number of terms of the series to add
+    printf("Enter N: ");
+    scanf("%d", &N);
+    printf("%.3f\n", harmonic(N));
+    return 0;
+}
