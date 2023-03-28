677392e8c3341680a9c08e47c1e1619d03f3fd64
nishkalprakash <nishkalprakash@gmail.com>
2023-03-28 16:03:11 +0530

    ready for checking

--- Assignments/A3/Q2/A3_Q2_solution.c
+++ Assignments/A3/Q2/A3_Q2_solution.c
@@ -1,8 +1,60 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;~~INPUT:~~123~~~~Expected OUTPUT:~~321~~~~;123
-10;~~INPUT:~~101~~~~Expected OUTPUT:~~101~~~~;101
-10;~~INPUT:~~993~~~~Expected OUTPUT:~~399~~~~;993
-10;~~INPUT:~~100~~~~Expected OUTPUT:~~1~~~~;100
-# ```
+/*
+    2.	A pair of numbers (X,Y) is called Perfect pair, if reversing the digits in one produces exactly the other numbers.  For Example: (56,65) is a perfect pair whereas (509,519) is not a perfect pair.
+    Write a program which will do the following:
+    a)	Read any two numbers in the range [-999,999] from the keyboard.
+    b)	Check if the two numbers form a perfect pair.
+    [10+20=30]
+*/
+// Code creator: Sipra Singh (sipra.singh@iitkgp.ac.in)
+#include<stdio.h>
+int main(){
+
+    int x, y;
+    printf("Enter the pair:\n");
+    scanf("%d%d",&x,&y);
+    //printf("x=%d,Y=%d",x,y);
+    int x1,x2, x3;
+    int y1,y2, y3;
+    x1=(int)(x%10);
+    x2=(int)((x/10)%10);
+    x3=(int)(((x/10)/10)%10);
+    //printf("x1=%d,x2=%d,x3=%d\n",x1,x2,x3);
+
+    y1=(int)(y%10);
+    y2=(int)((y/10)%10);
+    y3=(int)(((y/10)/10)%10);
+    //printf("y1=%d,y2=%d,y3=%d\n",y1,y2,y3);
+
+    if(x3!=0&&y3!=0){
+        //printf("In 1st if.\n");
+        if(x1==y3&&x2==y2&&x3==y1){
+            printf("Perfect Pair\n");
+        }
+        else{
+            printf("Not a perfect pair\n");
+        }
+    }
+    else if((x3==0&&y3==0)&&(x2!=0&&y2!=0)){
+        //printf("In 2nd if.\n");
+        if(x1==y2&&x2==y1){
+            printf("Perfect Pair\n");
+        }
+        else{
+            printf("Not a perfect pair\n");
+        }
+    }
+    else if((x2==0&&y2==0)&&(x3==0&&y3==0)){
+        //printf("In 3rd if.\n");
+        if(x1==y1){
+            printf("Perfect Pair\n");
+        }
+        else{
+            printf("Not a perfect pair\n");
+        }
+    }
+    else{
+        //printf("In else.\n");
+        printf("Wrong Input\n");
+    }
+    return 0;
+}
--- Assignments/A3/Q3/A3_Q3_solution.c
+++ Assignments/A3/Q3/A3_Q3_solution.c
@@ -1,8 +1,111 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;~~INPUT:~~123~~~~Expected OUTPUT:~~321~~~~;123
-10;~~INPUT:~~101~~~~Expected OUTPUT:~~101~~~~;101
-10;~~INPUT:~~993~~~~Expected OUTPUT:~~399~~~~;993
-10;~~INPUT:~~100~~~~Expected OUTPUT:~~1~~~~;100
-# ```
+/*
+    3.	A point in 3D space P(x,y,z) is defined as x,y and z coordinate values which are real numbers. Write a program to do the following:
+    a)	Read any three points A(x1,y1,z1), B(x2,y2,,z2) and C (x3,y3,z3) from the keyboard. Print the points on the screen.
+
+    b)	Check if the three points are collinear (i.e they lie on a straight line).
+
+    c)	Two ordered triplets are called in collating sequence (also called a sort sequence), if they are ordered in increasing order according to the order of positions of the elements in them. For example, (b, a, d) < (d, a, d); (m, a, p) < (m, a, t); (a, b, c) < (a, b, d), and so on.
+
+    Print the three points A, B and C in their collating sequence (i.e. if A<B<C etc.)
+*/
+// Code creator: Abhishek Topwal
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    double x1, y1, z1;
+    double x2, y2, z2;
+    double x3, y3, z3;
+
+    printf("A(x1,y1,z1) : ");
+    scanf("%lf %lf %lf", &x1, &y1, &z1);
+    printf("B(x2,y2,z2) : ");
+    scanf("%lf %lf %lf", &x2, &y2, &z2);
+    printf("C(x3,y3,z3) : ");
+    scanf("%lf %lf %lf", &x3, &y3, &z3);
+
+    // print the given three points
+    printf("A(x1,y1,z1) : (%.2lf,%.2lf,%.2lf)\n", x1, y1, z1);
+    printf("B(x2,y2,z2) : (%.2lf,%.2lf,%.2lf)\n", x2, y2, z2);
+    printf("C(x3,y3,z3) : (%.2lf,%.2lf,%.2lf)\n", x3, y3, z3);
+
+    /*
+    We can find the collinearity with three points in different methods such as:
+    (i) Using slope formula (slope of AB = slope of AC)
+    (ii) Using distance formula (AB AC BC AB+AC=BC)
+    (iii) Using area of triangle formula (area of triangle ABC = 0)
+    (iv) Using equation method (x1(y2-y3)+x2(y3-y1)+x3(y1-y2)=0)
+    (v) Using cross product method (AB X AC = 0)
+    */
+    // if the given three points (A,B,C) are collinear
+    // then cross product of vector AB and vector AC is zero
+
+    double x1_x2 = x1 - x2;
+    double y1_y2 = y1 - y2;
+    double z1_z2 = z1 - z2;
+
+    double x1_x3 = x1 - x3;
+    double y1_y3 = y1 - y3;
+    double z1_z3 = z1 - z3;
+
+    double i = y1_y2 * z1_z3 - z1_z2 * y1_y3;
+    double j = z1_z2 * x1_x3 - x1_x2 * z1_z3;
+    double k = x1_x2 * y1_y3 - y1_y2 * x1_x3;
+
+    if (i == 0 && j == 0 && k == 0)
+        printf("\nThe given three points are collinear.\n\n");
+    else
+        printf("\nThe given three points are not collinear.\n\n");
+
+    // print the points in collating sequence
+    double l1=-DBL_MAX,l2=-DBL_MAX ,l3 =-DBL_MAX ;
+
+      
+
+
+
+    if (x1 < x2 && x2 < x3)
+        printf("A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x2, y2, z2, x3, y3, z3);
+    else if (x1 < x3 && x3 < x2)
+        printf("A(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x3, y3, z3, x2, y2, z2);
+    else if (x2 < x1 && x1 < x3)
+        printf("B(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x2, y2, z2, x1, y1, z1, x3, y3, z3);
+    else if (x2 < x3 && x3 < x1)
+        printf("B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf)\n", x2, y2, z2, x3, y3, z3, x1, y1, z1);
+    else if (x3 < x1 && x1 < x2)
+        printf("C(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x2, y2, z2, x3, y3, z3);
+    else if (x3 < x2 && x2 < x1)
+        printf("C(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf)\n", x3, y3, z3, x2, y2, z2, x1, y1, z1);
+    else
+    {
+        if (y1 < y2 && y2 < y3)
+            printf("A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x2, y2, z2, x3, y3, z3);
+        else if (y1 < y3 && y3 < y2)
+            printf("A(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x3, y3, z3, x2, y2, z2);
+        else if (y2 < y1 && y1 < y3)
+            printf("B(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x2, y2, z2, x1, y1, z1, x3, y3, z3);
+        else if (y2 < y3 && y3 < y1)
+            printf("B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf)\n", x2, y2, z2, x3, y3, z3, x1, y1, z1);
+        else if (y3 < y1 && y1 < y2)
+            printf("C(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf)\n", x3, y3, z3, x1, y1, z1, x2, y2, z2);
+        else if (y3 < y2 && y2 < y1)
+            printf("C(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf)\n", x3, y3, z3, x2, y2, z2, x1, y1, z1);
+        else
+        {
+            if (z1 < z2 && z2 < z3)
+                printf("A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x2, y2, z2, x3, y3, z3);
+            else if (z1 < z3 && z3 < z2)
+                printf("A(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x3, y3, z3, x2, y2, z2);
+            else if (z2 < z1 && z1 < z3)
+                printf("B(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x2, y2, z2, x1, y1, z1, x3, y3, z3);
+            else if (z2 < z3 && z3 < z1)
+                printf("B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf)\n", x2, y2, z2, x3, y3, z3, x1, y1, z1);
+            else if (z3 < z1 && z1 < z2)
+                printf("C(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x2, y2, z2, x3, y3, z3);
+            else if (z3 < z2 && z2 < z1)
+                printf("C(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < A(%.2lf,%.2lf,%.2lf)\n", x3, y3, z3, x2, y2, z2, x1, y1, z1);
+            else
+                printf("A(%.2lf,%.2lf,%.2lf) < B(%.2lf,%.2lf,%.2lf) < C(%.2lf,%.2lf,%.2lf)\n", x1, y1, z1, x2, y2, z2, x3, y3, z3);
+        }
+    }
+}
