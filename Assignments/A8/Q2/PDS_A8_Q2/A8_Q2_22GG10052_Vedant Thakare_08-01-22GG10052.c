96d753a329a6de0e3b62cbefc05b627f5abf8000
nishkalprakash <nishkalprakash@gmail.com>
2023-06-02 19:59:54 +0530

    rechecked for A8_Q2 22GG10052

--- Assignments/A8/Q2/PDS_A8_Q2/A8_Q2_22GG10052_Vedant Thakare_08-01-22GG10052.c
+++ Assignments/A8/Q2/PDS_A8_Q2/A8_Q2_22GG10052_Vedant Thakare_08-01-22GG10052.c
@@ -1,66 +1,71 @@
-/*
-* Section 2
-* Roll no : 22GG10052
-* Name : Vedant Thakare
-* Assignment : 8
-* Description : Storing the data
-*/
 #include<stdio.h>
-typedef struct Student{
-char Rollno[10];
-float marks[3];
-float total;
-}stud;
-stud swap(stud x,stud y)
-{
-    stud temp=x;
-    x=y;
-    y=temp;
-}
+typedef struct matrix{
+int m;
+int n;
+}matrix;
 int main()
 {
-    int n,j;
-    printf("Enter the number of students\n");
-    scanf("%d",&n);
-    stud arr[n],temp;
-    for(int i=0;i<n;i++)
+    int m1,n1,m2,n2,k;
+    printf("Enter the number of rows for matrix A:\n");
+    scanf("%d",&m1);
+    printf("Enter the number of columns for matrix A:\n");
+    scanf("%d",&n1);
+    printf("Enter the number of rows for matrix B:\n");
+    scanf("%d",&m2);
+    printf("Enter the number of columns for matrix B:\n");
+    scanf("%d",&n2);
+    if(n1!=m2)
     {
-        printf("Enter the roll no for student %d\n",i+1);
-        scanf("%s",arr[i].Rollno);
-        printf("Enter the marks for 3 subjects\n");
-        scanf("%f %f %f",&arr[i].marks[0],&arr[i].marks[1],&arr[i].marks[2]);
-        arr[i].total=arr[i].marks[0]+arr[i].marks[1]+arr[i].marks[2];
+        printf("Invalid input");
     }
-    //printing the records
-    printf("Student records :\n");
-    for(int i=0;i<n;i++)
+    else{
+    int **A,**B,**C;
+    A = (int **) calloc(m1, sizeof(int*));
+    for(int i = 0; i < m1; i++)
+    A[i] = (int *) calloc(n1, sizeof(int));
+    B = (int **) calloc(m2, sizeof(int*));
+    for(int i = 0; i < m2; i++)
+    B[i] = (int *) calloc(n2, sizeof(int));
+    C= (int **) calloc(m1, sizeof(int*));
+    for(int i = 0; i < m1; i++)
+    C[i] = (int *) calloc(n2, sizeof(int));
+
+    printf("Enter the elements of matrix A :");
+    printf("\n");
+    int i,j;
+    for(i=0;i<m1;i++)
     {
-      printf("Roll No : %s\n",arr[i].Rollno);
-      printf("Marks : %.2f %.2f %.2f\n",arr[i].marks[0],arr[i].marks[1],arr[i].marks[2]);
-      printf("Total : %.2f\n",arr[i].total);
-    }
-    //swapping the structures
-    for(int i=0;i<n;i++)
-    {
-        for(j=i+1;j<n;j++)
+        for(j=0;j<n1;j++)
         {
-            if(arr[i].total<arr[j].total)
-            {
-                   temp=arr[i];
-                    arr[i]=arr[j];
-                    arr[j]=temp;
-
-            }
+            scanf("%d",&A[i][j]);
         }
-
     }
-    printf("Student records :\n");
-    for(int i=0;i<n;i++)
+    printf("Enter the elements of matrix B :");
+    printf("\n");
+    for(i=0;i<n1;i++)
     {
-      printf("Roll No : %s\n",arr[i].Rollno);
-      printf("Marks : %.2f %.2f %.2f\n",arr[i].marks[0],arr[i].marks[1],arr[i].marks[2]);
-      printf("Total : %.2f\n",arr[i].total);
+        for(j=0;j<n2;j++)
+        {
+            scanf("%d",&B[i][j]);
+        }
     }
-
+    for(i=0;i<m1;i++)
+    {
+        for(j=0;j<n2;j++)
+        {
+            for(k=0;k<n1;k++)
+                C[i][j]+=A[i][k]*B[k][j];
+        }
+    }
+    printf("Matrix C :");
+    printf("\n");
+    for(i=0;i<m1;i++)
+    {
+        for(j=0;j<n2;j++)
+        {
+            printf("%d ",C[i][j]);
+        }
+        printf("\n");
+    }}
     return 0;
 }
