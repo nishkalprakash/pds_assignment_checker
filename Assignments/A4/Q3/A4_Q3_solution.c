13f68fb4a25a0aee31ad304b6185d0de66839c9e
Alka Ranjan <alkarnjn@gmail.com>
2023-09-08 17:12:01 +0530

    done checking for A4_Q3

--- Assignments/A4/Q3/A4_Q3_solution.c
+++ Assignments/A4/Q3/A4_Q3_solution.c
@@ -1,9 +1,26 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;321;123
-10;101;101
-10;993;993
-10;100;100
-10;100;100
-# ```
+#include <stdio.h>
+
+int main() {
+    int n, reversedN = 0, originalN, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    originalN = n;  // Store the original number
+
+    // Reverse the number
+    while (n != 0) {
+        remainder = n % 10;
+        reversedN = reversedN * 10 + remainder;
+        n /= 10;
+    }
+
+    // Check if the original number is equal to its reverse
+    if (originalN == reversedN) {
+        printf("%d is a palindrome.\n", originalN);
+    } else {
+        printf("%d is not a palindrome.\n", originalN);
+    }
+
+    return 0;
+}
