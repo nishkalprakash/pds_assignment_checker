c1785adcc8fa14c54a9703af180bc9ce49056c6b
nishkalprakash <nishkalprakash@gmail.com>
2024-10-01 15:43:40 +0530

    added questions for A2-7

--- Assignments/A7/Q1/A7_Q1_solution.c
+++ Assignments/A7/Q1/A7_Q1_solution.c
@@ -1,9 +1,28 @@
-# Format: `{marks};{label};{test_case}`
-# * For Example:
-# ```csv
-10;321;123
-10;101;101
-10;993;993
-10;100;100
-10;100;100
-# ```
+// Code creator: Nishkal Prakash (nishkal@iitkgp.ac.in)
+// Program to print Sum of a series for first n terms
+
+#include <stdio.h>
+int power(int x, int n) // Calculates x^n and returns an integer
+{
+	long xx = 1; // for n=0 power() returns 1
+	while (n--)
+		xx *= x;
+	return xx;
+}
+int fact(int n) // Calculates n! and returns an integer
+{
+	long f = 1;	// for n=0 fact() returns 1
+	while (n--)
+		f *= (n + 1);
+	return f;
+}
+int main()
+{
+	int X, N; // N is the input number
+	double sum = 0;
+	printf("Enter X and N: ");
+	scanf("%d%d", &X, &N);
+	while (N--)
+		sum += ((double)power(X, N)) / fact(N); // Type casting to double/float to prevent integer division
+	printf("%lf", sum);
+}
